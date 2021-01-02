c4a1576f37d37d37a027a0c5e86bf1f404d067e3
Yuriy Paramonov <yuriy.paramonov@outlook.com>
2021-01-03 00:24:42 +0200

    Unimportant

---
+++ HrefExtruder/Program.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Xml;
+
+namespace HrefExtruder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static void ExtractHref(string URL)
+        {
+            // declaring & loading dom
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc = web.Load(URL);
+
+    // extracting all links
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+    {
+                HtmlAttribute att = link.Attributes["href"];
+                // showing output
+                Console.WriteLine(att.Value);
+            }
+        }
+    }
+}
--- MouseMover/Program.cs
+++ MouseMover/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+    using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Afkvalley;
 
 namespace MouseMover
 {
@@ -10,6 +12,16 @@ namespace MouseMover
     {
         static void Main(string[] args)
         {
+            if (args == null)
+                new Exception("Should be some args");
+            if(args.Length!=3)
+                 new Exception("Should be 3 args x and y and mouse");
+
+            int x = Int32.Parse(args[0]);
+            int y = Int32.Parse(args[1]);
+            WinAPI.MouseMove(x,y);
+            WinAPI.MouseClick(args[2]);
+            Console.WriteLine($"{args[2]} click x:{x} y:{y}");
         }
     }
 }
